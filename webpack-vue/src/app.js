745ea8ffac04171fb7e311ab808700aa5eb87adb
rongbin.xie <xrb16302@163.com>
2019-07-29 08:39:19 +0800

    + 修改vuex 记录登录信息会刷新的问题，但是还有个问题，那么就是在新标签中打开会失去登录信息

--- webpack-vue/src/app.js
+++ webpack-vue/src/app.js
@@ -6,18 +6,51 @@ Vue.use(Vuex)
 
 let store = new Vuex.Store({
   state: {
-    name: '',
-    count: 1
+    count: 1,
+    username: '',
+    isLogin: false
   },
   //不支持异步处理，可以在外部直接通过store.commit("methdoType",optionParamObj)的方式来调用
   mutations: {
-    increment (state) {
-      state.count++
+    increment (state, payload) {
+      state.count = state.count + payload.inc
+    },
+    login (state, userInfo) {
+      state.username = userInfo.username
+    },
+    //mutations
+    ADD_LOGIN_USER (state, data) {  //登入，保存状态
+      sessionStorage.setItem('username', data)  //添加到sessionStorage
+      sessionStorage.setItem('isLogin', true)
+      state.username = data             //同步的改变store中的状态
+      state.isLogin = true
+    },
+    SIGN_OUT (state) {   //退出，删除状态
+      sessionStorage.removeItem('username')  //移除sessionStorage
+      sessionStorage.removeItem('isLogin')
+      state.username = ''                //同步的改变story中的状态
+      state.isLogin = false
     }
   },
   //调用mutation的方式来更新state的状态，但是支持异步
   actions: {},
-  getters: {}
+  getters: {
+    //getters
+    isLogin (state) {
+      if (!state.isLogin) {
+        state.isLogin = sessionStorage.getItem('isLogin')   //从sessionStorage中读取状态
+        state.username = sessionStorage.getItem('username')
+      }
+      return state.isLogin
+    },
+    username (state) {
+      if (!state.isLogin) {
+        state.isLogin = sessionStorage.getItem('isLogin')   //从sessionStorage中读取状态
+        state.username = sessionStorage.getItem('username')
+      }
+      return state.username
+    }
+  }
 })
 
 new Vue({
--- webpack-vue/src/components/HelloComponent.vue
+++ webpack-vue/src/components/HelloComponent.vue
@@ -2,7 +2,9 @@
     <div>
         <h1>{{count}}</h1>
         <h2>hello, {{name}}</h2>
-        <button v-on:click="addCount()">add</button>
+        <button v-on:click="addCount(2)">add2</button>
+        <button v-on:click="login()">login</button>
+        <button v-on:click="logOut()">loginOut</button>
     </div>
 </template>
 <script>
@@ -19,11 +21,20 @@
       return {}
     },
     methods: {
-      addCount: function () {
-        this.$store.commit('increment')
+      addCount: function (inc) {
+        //commit with payload
+        // this.$store.commit('increment', {inc: inc})
+        //object-style commit
+        this.$store.commit({
+          type: 'increment',
+          inc: inc
+        })
       },
-      custom () {
-        alert('hello,world')
+      login: function () {
+        this.$store.commit('ADD_LOGIN_USER', 'asa.x')
+      },
+      logOut () {
+        this.$store.commit('SIGN_OUT')
       }
     },
     computed: {
